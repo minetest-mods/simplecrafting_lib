e7242974a6a95af3945a38e3f6d070f3cbd98ead
FaceDeer <derksenmobile@gmail.com>
2017-04-30 09:53:26 -0600

    Bring back Qwerty's original furnace

--- init.lua
+++ init.lua
@@ -6,9 +6,9 @@ local modpath = minetest.get_modpath("crafting")
 
 dofile(modpath .. "/config.lua")
 dofile(modpath .. "/util.lua")
+dofile(modpath .. "/legacy.lua")
 dofile(modpath .. "/table.lua")
 dofile(modpath .. "/furnace.lua")
-dofile(modpath .. "/legacy.lua")
 
 
 -- Hopper compatibility
