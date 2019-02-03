69e9e3a4372faa5c64c2efc6018b837216c376f2
FaceDeer <derksenmobile@gmail.com>
2019-02-03 16:40:14 -0700

    add option to override player crafting directly

--- init.lua
+++ init.lua
@@ -21,3 +21,17 @@ dofile(modpath .. "/postprocessing.lua")
 if minetest.settings:get_bool("simplecrafting_lib_enable_developer_commands") then
 	dofile(modpath .. "/saveload/saveload.lua")
 end
+
+if minetest.settings:get_bool("simplecrafting_lib_override_default_player_crafting") then
+	simplecrafting_lib.register_recipe_import_filter(function(legacy_recipe)
+		if legacy_recipe.input["simplecrafting_lib:heat"] then
+			return nil, false
+		elseif legacy_recipe.output and legacy_recipe.output:get_name() == "simplecrafting_lib:heat" then
+			return nil, false
+		else
+			return "player", true
+		end
+	end)
+
+	simplecrafting_lib.register_player_craft_type("player")
+end
